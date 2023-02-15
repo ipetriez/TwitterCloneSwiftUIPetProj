0f7f418cc12b450f08087798dadee67a376d8fea
Sergey Petrosyan <ipetriez@icloud.com>
2023-02-15 18:55:24 +0400

    Initial Commit

---
+++ TwitterCloneSwiftUIPetProj/TwitterCloneSwiftUIPetProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIPetProjApp.swift
+//  TwitterCloneSwiftUIPetProj
+//
+//  Created by Sergey Petrosyan on 15.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIPetProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
