94cc2517a47db0aba26798be0ead872a0aacf3be
Sergey Petrosyan <ipetriez@icloud.com>
2023-03-15 21:06:53 +0400

    Add floating button in FeedView.

--- TwitterCloneSwiftUIPetProj/Feed/FeedView.swift
+++ TwitterCloneSwiftUIPetProj/Feed/FeedView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct FeedView: View {
+    
+    @State private var showNewTweetView = false
+    
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0..<20) { _ in
-                    TweetRowView()
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0..<20) { _ in
+                        TweetRowView()
+                    }
                 }
+            }
+            
+            Button {
+                showNewTweetView.toggle()
+            } label: {
+                Image(systemName: "highlighter")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 28, height: 28)
+                    .padding()
+            }
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+            .fullScreenCover(isPresented: $showNewTweetView) {
+                Text("New tweet view")
             }
         }
     }
