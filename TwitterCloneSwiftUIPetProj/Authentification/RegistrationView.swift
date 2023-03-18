97461b33059789649c767249b22b60818f49b5c0
Sergey Petrosyan <ipetriez@icloud.com>
2023-03-18 19:25:59 +0400

    Create Log in screen.

---
+++ TwitterCloneSwiftUIPetProj/Authentification/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  TwitterCloneSwiftUIPetProj
+//
+//  Created by Sergey Petrosyan on 18.03.23.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
