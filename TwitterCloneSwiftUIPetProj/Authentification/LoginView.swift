3397ecb61031d438ef7cc9afa89547e84e18fda4
Sergey Petrosyan <ipetriez@icloud.com>
2023-03-20 19:27:33 +0400

    Create RegistrationView.

--- TwitterCloneSwiftUIPetProj/Authentification/LoginView.swift
+++ TwitterCloneSwiftUIPetProj/Authentification/LoginView.swift
@@ -38,18 +38,9 @@ struct LoginView: View {
                 }
             }
             
-            Button {
-                print("Sign in")
-            } label: {
-                Text("Sign in")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(width: 340, height: 50)
-                    .background(Color(.systemBlue))
-                    .clipShape(Capsule())
-                    .padding()
+            CapsuleShapedButton(title: "Sign in") {
+                print("Signing in")
             }
-            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
             
             Spacer()
             
--- TwitterCloneSwiftUIPetProj/Authentification/RegistrationView.swift
+++ TwitterCloneSwiftUIPetProj/Authentification/RegistrationView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @State private var email = ""
+    @State private var username = ""
+    @State private var fullName = ""
+    @State private var password = ""
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         VStack {
             AuthHeaderView(title1: "Get started.", title2: "Create your account.")
+            
+            VStack(spacing: 40) {
+                CustomInputField(imageName: "envelope", placeholderText: "Email", text: $email)
+                
+                CustomInputField(imageName: "person", placeholderText: "User name", text: $username)
+                
+                CustomInputField(imageName: "person", placeholderText: "Full name", text: $fullName)
+                
+                CustomInputField(imageName: "lock", placeholderText: "Password", text: $password)
+            }
+            .padding(32)
+            
+            CapsuleShapedButton(title: "Sign up") {
+                print("Signing up")
+            }
+            
+            Spacer()
+            
+            Button {
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                HStack {
+                    Text("Already have an account?")
+                        .font(.footnote)
+
+                    
+                    Text("Sign In")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+            .foregroundColor(Color(.systemBlue))
+
         }
+        .ignoresSafeArea()
     }
 }
 
